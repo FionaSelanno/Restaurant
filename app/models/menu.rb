1f1860d13ca614f54857450ec405d0c267258181
ramonavic <ramon.hofmeister@gmail.com>
2016-06-16 14:56:13 +0200

    Added search function in menu model

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,5 +1,11 @@
 class Menu < ActiveRecord::Base
-\
+
   belongs_to :location
   has_many :bookings
+
+
+    def self.search(search)
+      where("restaurant_name ILIKE ? OR city ILIKE ? OR description ILIKE ?", "%#{search}%", "%#{search}%", "%#{search}%"  )
+    end
+
 end
