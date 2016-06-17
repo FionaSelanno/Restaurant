eab70c81db0e6c483f4e06389e309dafe03a000c
ramonavic <ramon.hofmeister@gmail.com>
2016-06-17 11:25:28 +0200

    Fixed menu search

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,6 @@ Rails.application.routes.draw do
 
   resources :users, only: [:show]
   resources :profiles
-  get "search" => "search_results#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
