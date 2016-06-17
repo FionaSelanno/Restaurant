25efa5a536bb9e050e39fcdb570fbb569eec9709
“FionaSelanno” <fcs_fiona@hotmail.com>
2016-06-17 16:22:13 +0200

    Config application edit

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,4 @@
 require File.expand_path('../boot', __FILE__)
-config.assets.initialize_on_precompile = false
 
 require 'rails/all'
 
@@ -9,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module Restaurant
   class Application < Rails::Application
+    config.assets.initialize_on_precompile = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
